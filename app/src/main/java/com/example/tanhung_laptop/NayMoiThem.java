f0755de0ce87a89299ddc398761767827afdb91e
DinhNe <nguyenquangdinh1408@gmail.com>
2022-07-19 16:09:57 +0700

    19/07

---
+++ app/src/main/java/com/example/tanhung_laptop/NayMoiThem.java
@@ -0,0 +1,14 @@
+package com.example.tanhung_laptop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NayMoiThem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nay_moi_them);
+    }
+}
